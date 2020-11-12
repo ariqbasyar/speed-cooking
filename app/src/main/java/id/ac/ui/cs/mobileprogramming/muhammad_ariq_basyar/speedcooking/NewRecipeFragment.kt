1056c0c391d22ef7722631c0e80675684f3fd3f7
ariqbasyar <ariqbasyar2@gmail.com>
2020-11-12 21:19:45 +0700

    pick image, dynamic add input for ingredients and added user permissions on manifest

--- app/src/main/java/id/ac/ui/cs/mobileprogramming/muhammad_ariq_basyar/speedcooking/NewRecipeFragment.kt
+++ app/src/main/java/id/ac/ui/cs/mobileprogramming/muhammad_ariq_basyar/speedcooking/NewRecipeFragment.kt
@@ -1,22 +1,34 @@
 package id.ac.ui.cs.mobileprogramming.muhammad_ariq_basyar.speedcooking
 
+import android.annotation.SuppressLint
+import android.app.Activity
 import android.content.Intent
+import android.os.Build
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
+import android.widget.LinearLayout
+import android.widget.Toast
+import androidx.core.content.PermissionChecker
+import androidx.core.view.children
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProviders
 import dagger.hilt.android.AndroidEntryPoint
 import id.ac.ui.cs.mobileprogramming.muhammad_ariq_basyar.speedcooking.databinding.NewRecipeFragmentBinding
 import id.ac.ui.cs.mobileprogramming.muhammad_ariq_basyar.speedcooking.viewmodels.NewRecipeViewModels
+import kotlinx.android.synthetic.main.new_recipe_fragment.*
 
 const val PICK_IMAGE = 100
+const val PERMISSION_CODE = 101
 
 @AndroidEntryPoint
 class NewRecipeFragment: Fragment() {
+
     private lateinit var binding: NewRecipeFragmentBinding
+    private lateinit var ingredientList: LinearLayout
     private val newRecipeViewModels: NewRecipeViewModels by lazy {
         ViewModelProviders.of(this).get(NewRecipeViewModels::class.java)
     }
@@ -32,6 +44,7 @@ class NewRecipeFragment: Fragment() {
             container,
             false
         )
+        ingredientList = binding.addIngredientLinearLayout
         return binding.root
     }
 
@@ -40,12 +53,80 @@ class NewRecipeFragment: Fragment() {
 
         binding.viewModel = newRecipeViewModels
 
-        binding.button.setOnClickListener {
+        binding.saveRecipeButton.setOnClickListener {
             save()
+        }
+        binding.addIngredientButton.setOnClickListener {
+            addIngredient()
+        }
+
+        binding.uploadPictureButton.setOnClickListener {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                if (context?.let { it1 ->
+                        PermissionChecker.checkSelfPermission(it1,android.Manifest.permission.READ_EXTERNAL_STORAGE)
+                    } == PermissionChecker.PERMISSION_DENIED) {
+                    val permissions = arrayOf(android.Manifest.permission.READ_EXTERNAL_STORAGE)
+                    requestPermissions(permissions, PERMISSION_CODE)
+                } else {
+                    pickImage()
+                }
+            } else {
+                pickImage()
+            }
+        }
+
+        newRecipeViewModels.imageUrl.observe(viewLifecycleOwner) {
+            selected_image_view.setImageURI(it)
+        }
+    }
+
+    private fun pickImage() {
+        val intent = Intent(Intent.ACTION_PICK)
+        intent.type = "image/*"
+        startActivityForResult(intent, PICK_IMAGE)
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when(requestCode) {
+            PERMISSION_CODE -> {
+                if (grantResults.size > 0 &&
+                    grantResults[0] == PermissionChecker.PERMISSION_GRANTED) {
+                    pickImage()
+                } else {
+                    Toast.makeText(context, "Permission denied.", Toast.LENGTH_LONG).show()
+                }
+            }
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (resultCode == Activity.RESULT_OK && requestCode == PICK_IMAGE) {
+            newRecipeViewModels.setImageUrl(data?.data)
+        }
+    }
+
+    @SuppressLint("InflateParams")
+    private fun addIngredient() {
+        val ingredientInput: View =
+            layoutInflater.inflate(R.layout.new_ingredient_input_text, null, false)
+        ingredientList.addView(ingredientInput)
+    }
+
+    private fun saveIngredientsEditText() {
+        ingredientList.children.iterator().forEach {
+            val editText: EditText = it.findViewById(R.id.ingredient_edit_text)
+            newRecipeViewModels.addIngredient(editText.text.toString())
         }
     }
 
     private fun save() {
+        saveIngredientsEditText()
         newRecipeViewModels.save()
         val backToMain = Intent(context, MainActivity::class.java)
         startActivity(backToMain)
--- app/src/main/java/id/ac/ui/cs/mobileprogramming/muhammad_ariq_basyar/speedcooking/viewmodels/NewRecipeViewModels.kt
+++ app/src/main/java/id/ac/ui/cs/mobileprogramming/muhammad_ariq_basyar/speedcooking/viewmodels/NewRecipeViewModels.kt
@@ -1,21 +1,37 @@
 package id.ac.ui.cs.mobileprogramming.muhammad_ariq_basyar.speedcooking.viewmodels
 
+import android.app.Application
+import android.net.Uri
 import android.util.Log
 import androidx.hilt.lifecycle.ViewModelInject
-import androidx.lifecycle.ViewModel
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.MutableLiveData
+import id.ac.ui.cs.mobileprogramming.muhammad_ariq_basyar.speedcooking.data.ingredient.IngredientRepository
 import id.ac.ui.cs.mobileprogramming.muhammad_ariq_basyar.speedcooking.data.recipe.RecipeRepository
 
-class NewRecipeViewModels @ViewModelInject internal constructor (
-    recipeRepository: RecipeRepository
-): ViewModel() {
+class NewRecipeViewModels @ViewModelInject internal constructor(
+    recipeRepository: RecipeRepository,
+    ingredientRepository: IngredientRepository,
+    application: Application
+): AndroidViewModel(application) {
 
-    var name: String = ""
-    var procedure: String = ""
-    var image_url: String = ""
+    var name = ""
+    var procedure = ""
+    private var ingredients = ArrayList<String>()
+    var imageUrl = MutableLiveData<Uri>()
+
+    fun setImageUrl(imageUrl: Uri?) {
+        this.imageUrl.postValue(imageUrl)
+    }
+
+    fun addIngredient(ingredient: String) {
+        ingredients.add(ingredient)
+    }
 
     fun save() {
-        Log.d("ASDASD", "submit name: $name")
-        Log.d("ASDASD", "submit procedure: $procedure")
-        Log.d("ASDASD", "submit image url: $image_url")
+        Log.d("ASDASD", "save name: $name")
+        Log.d("ASDASD", "save procedure: $procedure")
+        Log.d("ASDASD", "save ingredient: ${ingredients.joinToString()}")
+        Log.d("ASDASD", "save image url: ${imageUrl.value.toString()}")
     }
 }
