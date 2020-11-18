7b3c3403446deef940bcc84aeb7d4b113f06b75c
ariqbasyar <ariqbasyar2@gmail.com>
2020-11-18 07:38:34 +0700

    extract "choose an app" string to string resources

--- app/src/main/java/id/ac/ui/cs/mobileprogramming/muhammad_ariq_basyar/speedcooking/ui/recipe/PopUpFragment.kt
+++ app/src/main/java/id/ac/ui/cs/mobileprogramming/muhammad_ariq_basyar/speedcooking/ui/recipe/PopUpFragment.kt
@@ -92,7 +92,7 @@ class PopUpFragment : Fragment() {
                 shareIntent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION) // temp permission for receiving app to read this file
                 shareIntent.setDataAndType(contentUri, context!!.contentResolver.getType(contentUri))
                 shareIntent.putExtra(Intent.EXTRA_STREAM, contentUri)
-                startActivity(Intent.createChooser(shareIntent, "Choose an app"))
+                startActivity(Intent.createChooser(shareIntent, resources.getString(R.string.choose_an_app)))
             }
         } catch (e: IOException) {
             e.printStackTrace()
